72b2c48501ed0f0408e99e6da259dd070c21f651
Tyler Bletsch <rangita@eml.cc>
2014-10-05 13:46:44 -0400

    basic wget works!

--- uarlWIFI/uarlWIFI.cpp
+++ uarlWIFI/uarlWIFI.cpp
@@ -6,9 +6,15 @@
  */
 #include <uarlWIFI.h>
 
-WIFI::WIFI(void) {
-    //SERIAL_WIFI.println("constr");
+WIFI::WIFI() {
+    //SerialWifi->println("constr");
     dbg = NULL;
+}
+
+void WIFI::begin(HardwareSerial& serial) {
+    SerialWifi = &serial;
+    SerialWifi->begin(115200);
+    SerialWifi->setTimeout(SERIAL_WIFI_TIMOUT);
 }
 
 // allows debug_println, etc. after set_debug_stream has been called to provide a stream (e.g. a SoftwareSerial object)
@@ -16,7 +22,7 @@ WIFI::WIFI(void) {
 #define debug_printhex(s)   { if (dbg) dbg->print(s,HEX); }
 #define debug_println(s) { if (dbg) dbg->println(s); }
 
-#define DEBUG_EXPECT 0
+#define DEBUG_EXPECT 0 // enable to dump the data read by an expect operation
 
 void WIFI::set_debug_stream(Stream& s) {
     dbg = &s;
@@ -25,8 +31,17 @@ void WIFI::set_debug_stream(Stream& s) {
 bool WIFI::command(char* cmd, char* exp1, char* exp2, int timeout) {
     debug_print("> ");
     debug_println(cmd);
-    SERIAL_WIFI.println(cmd);
-    SERIAL_WIFI.flush();
+    SerialWifi->println(cmd); // implies CRLF, which is basically okay, except it screws up CIPSEND, since the device considers CR to be the break, so the NL is counted as part of data
+    SerialWifi->flush();
+    return expect(exp1,exp2,timeout);
+}
+
+bool WIFI::command_cr(char* cmd, char* exp1, char* exp2, int timeout) {
+    debug_print("*> ");
+    debug_println(cmd);
+    SerialWifi->print(cmd); // CR only, needed for CIPSEND command (but otherwise considered annoying)
+    SerialWifi->print("\r");
+    SerialWifi->flush();
     return expect(exp1,exp2,timeout);
 }
 
@@ -36,8 +51,8 @@ bool WIFI::expect(char* exp1, char* exp2, int timeout) {
     String data;
     int tStart = millis();
     while (millis() < tStart+timeout) {
-        if (SERIAL_WIFI.available()) {
-            char c = SERIAL_WIFI.read();
+        if (SerialWifi->available()) {
+            char c = SerialWifi->read();
             data += c;
             if (c==*e1) {
                 e1++;
@@ -70,7 +85,7 @@ bool WIFI::expect(char* exp1, char* exp2, int timeout) {
         }
     }
     debug_println("< no")
-#if DEBUG_EXPECT
+#if 1 || DEBUG_EXPECT
     debug_print("no:<<")
     debug_print(data);
     debug_println(">>")
@@ -82,8 +97,6 @@ bool WIFI::expect(char* exp1, char* exp2, int timeout) {
 
 bool WIFI::Initialize(wifi_mode_t mode, char* ssid, char* pwd, byte channel, wifi_enc_t enc) {
     debug_println("init");
-    SERIAL_WIFI.begin(115200);
-    SERIAL_WIFI.setTimeout(SERIAL_WIFI_TIMOUT);
     
     if (!command("AT+RST","ready\r\n")) {
         debug_println("error: unable to reset module");
@@ -123,381 +136,50 @@ bool WIFI::Initialize(wifi_mode_t mode, char* ssid, char* pwd, byte channel, wif
 	return true;
 }
 
-/*void WIFI::ipConfig(byte type, String addr, int port, boolean a, byte id) {
-	if (a == 0 ) {
-		confMux(a);
-		long timeStart = millis();
-		while (1) {
-			long time0 = millis();
-			if (time0 - timeStart > 5000) {
-				break;
-			}
-		}
-		newMux(type, addr, port);
-	}
-	else if (a == 1) {
-		confMux(a);
-		long timeStart = millis();
-		while (1) {
-			long time0 = millis();
-			if (time0 - timeStart > 5000) {
-				break;
-			}
-		}
-		newMux(id, type, addr, port);
-	}
+bool WIFI::tcp_connect(char* ip_address, unsigned short port) {
+    char cmd[64];
+    sprintf(cmd,"AT+CIPSTART=\"TCP\",\"%s\",%d",ip_address,port);
+    return command(cmd, "Linked");
 }
-*/
-
-int WIFI::ReceiveMessage(char *buf, int MsgLen) {
-	//+IPD:11,wifi read11
-	//done
-	String data = "";
-	if (SERIAL_WIFI.available()>0) {
-		//Serial.println(SERIAL_WIFI.available());
-		char c0 = SERIAL_WIFI.read();
-		if (c0 == '+') {
-			while (1) {
-				if (SERIAL_WIFI.available()>0) {
-					char c = SERIAL_WIFI.read();
-					data += c;
-				}
-				if (data.indexOf("done")!=-1) {
-					break;
-				}
-			}
-			//Serial.println(data);
-			int sLen = strlen(data.c_str());
-			int i;
-			for (i = 0; i <= sLen; i++) {
-				if (data[i] == ':') {
-					break;
-				}
-				
-			}
-			String _size = data.substring(4, i);
-			int iSize = _size.toInt();
-			//Serial.println(_size);
-			String str = data.substring(i+1, i+1+iSize);
-			strcpy(buf, str.c_str());
-			//Serial.println(str);
-			return iSize;
-		}
-	}
-	
-	return 0;
+#define eatspace { \
+    while (1) { \
+        char c = SerialWifi->peek(); \
+        if (c=='\r' || c=='\n') SerialWifi->read(); \
+        else break; \
+    } \
 }
 
-//////////////////////////////////////////////////////////////////////////
 
-
-/*********************************************
- *********************************************
- *********************************************
-                   TPC/IP功能指令
- *********************************************
- *********************************************
- *********************************************
- */
-
-
-/*==============================================*/
-/*
- *
- * 获取目前的链接状态
- * <ID>  0-4
- * <type>  tcp or udp
- * <addr>  ip
- * <port>  port number
- * /
- /*============================================*/
-String WIFI::showStatus(void) {
-    SERIAL_WIFI.println("AT+ShowSTA");  //发送AT指令
-      String data;
-      while (1) {
-       if(SERIAL_WIFI.available()>0) {
-       char a =SERIAL_WIFI.read();
-       data=data+a;
-       }
-       if (data.indexOf("done")!=-1) {
-           break;
-       }
+bool WIFI::send(char* data) {
+    char cmd[32];
+    sprintf(cmd,"AT+CIPSEND=%d",strlen(data));
+    if (!command_cr(cmd, "> ")) {
+        debug_println("error: unable to begin send");
+        return false;
     }
-
-          char head[4] = {0x0D,0x0A};   //头部多余字符串
-          char tail[7] = {0x0D,0x0A,0x0D,0x0A};        //尾部多余字符串
-          data.replace("AT+ShowSTA","");
-          data.replace("done","");
-          data.replace(head,"");
-          data.replace(tail,"");
-          return data;
-}
-
-/*============================================*/
-/*
- * 查询目前的链接模式（单链接or多链接）
- * */
-/*============================================*/
-String WIFI::showMux(void) {
-    String data;
-    SERIAL_WIFI.println("AT+MUX?");  //发送AT指令
-
-      while (1) {
-       if(SERIAL_WIFI.available()>0) {
-       char a =SERIAL_WIFI.read();
-       data=data+a;
-       }
-       if (data.indexOf("done")!=-1) {
-           break;
-       }
+    SerialWifi->write(data);
+    SerialWifi->flush();
+    if (!expect("SEND OK")) {
+        debug_println("error: got bad response from actual send");
+        return false;
     }
-          char head[3]={0x0D,0x0A};   //将回显数据处理成需要的格式
-          data.replace("AT+MUX?","");
-          data.replace("+MUX","showMux");
-          data.replace("done","");
-          data.replace(head,"");
-          return data;
+    return true;
 }
 
-/*============================================*/
-/*
- * 设置链接模式（单链接or多链接）
- * */
-/*============================================*/
-void WIFI::confMux(boolean a) {
- SERIAL_WIFI.print("AT+MUX=");
- SERIAL_WIFI.println(a);           //发送AT指令
- while (1) {                            //当串口有完成数据返回时，结束语句
-     if(SERIAL_WIFI.find("done")==true || SERIAL_WIFI.find("Link is builded")==true) {
-        break;
-     }
-  }
-}
-
-
-/*============================================*/
-/*
- * 建立tcp链接或者注册UDP端口号（单路模式）
- *
- * */
-/*===========================================*/
-void WIFI::newMux(byte type, String addr, int port) {
-    String data;
-    SERIAL_WIFI.print("AT+NewSTA=");
-    if(type>0) {
-        SERIAL_WIFI.print("\"TCP\"");
-    }else
-    {
-        SERIAL_WIFI.print("\"UDP\"");
-    }
-    SERIAL_WIFI.print(",");
-    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.print(addr);
-    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.print(",");
-//    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.println(String(port));
-//    SERIAL_WIFI.println("\"");
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("done")!=-1 || data.indexOf("ALREAY CONNECT")!=-1 || data.indexOf("ERROR")!=-1) {
-         break;
-     }
-  }
-}
-/*============================================*/
-/*
- * 建立tcp链接或者注册UDP端口号（多路模式）（0-4共计5路）
- *
- * */
-/*===========================================*/
-void WIFI::newMux( byte id, byte type, String addr, int port) {
-
-    SERIAL_WIFI.print("AT+NewSTA=");
-    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.print(String(id));
-    SERIAL_WIFI.print("\"");
-    if(type>0) {
-        SERIAL_WIFI.print("\"tcp\"");
-    }else
-    {
-        SERIAL_WIFI.print("\"UDP\"");
-    }
-    SERIAL_WIFI.print(",");
-    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.print(addr);
-    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.print(",");
-//    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.println(String(port));
-//    SERIAL_WIFI.println("\"");
-    String data;
-    while (1) {
-
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("done")!=-1 || data.indexOf("ALREAY CONNECT")!=-1 || data.indexOf("ERROR")!=-1) {
-         break;
-     }
-  }
-
-}
-/*==============================================*/
-/*
- * 发送数据(单路模式)
- *
- * */
-/*==============================================*/
-void WIFI::Send(String str) {
-    SERIAL_WIFI.print("AT+UpDate=");
-//    SERIAL_WIFI.print("\"");
-    SERIAL_WIFI.println(str.length());
-//    SERIAL_WIFI.println("\"");
-    while (1) {                            //当串口有完成数据返回时，结束语句
-        if(SERIAL_WIFI.find(">")==true ) {
-           break;
-        }
-     }
-    SERIAL_WIFI.println(str);
-
-
-    String data;
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("SEND OK")!=-1) {
-         break;
-     }
-  }
-}
-
-/*==============================================*/
-/*
- * 发送数据(多路路模式)
- *
- * */
-/*==============================================*/
-void WIFI::Send(byte id, String str) {
-    SERIAL_WIFI.print("AT+UpDate=");
-
-    SERIAL_WIFI.print(String(id));
-    SERIAL_WIFI.println(",");
-    SERIAL_WIFI.println(str.length());
-    while (1) {                            //当串口有完成数据返回时，结束语句
-        if(SERIAL_WIFI.find(">")==true ) {
-           break;
-        }
-     }
-    SERIAL_WIFI.println(str);
-
-
-    String data;
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("SEND OK")!=-1) {
-         break;
-     }
-  }
-}
-
-/*========================================*/
-/*
- *
- * 关闭TCP或UDP链接（单路模式）
- *
- * *-*/
-/*=======================================*/
-void WIFI::closeMux(void) {
-    SERIAL_WIFI.println("AT+CLOSE");
-
-    String data;
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("Linked")!=-1 || data.indexOf("ERROR")!=-1 || data.indexOf("we must restart")!=-1) {
-         break;
-     }
-  }
-}
-
-
-/*========================================*/
-/*
- *
- * 关闭TCP或UDP链接（多路模式）
- *
- * *-*/
-/*=======================================*/
-void WIFI::closeMux(byte id) {
-    SERIAL_WIFI.print("AT+CLOSE=");
-    SERIAL_WIFI.println(String(id));
-    String data;
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("done")!=-1 || data.indexOf("Link is not")!=-1 || data.indexOf("Cant close")!=-1) {
-         break;
-     }
-  }
-
-}
-
-/*=========================================*/
-/*
- * 获取目前的本机IP地址
- * */
-/*=========================================*/
-String WIFI::showIP(void) {
-    SERIAL_WIFI.println("AT+ShowIP");  //发送AT指令
-    String data;
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("done")!=-1) {
-         break;
-     }
-  }
-    return data;
-}
-
-/*=======================================*/
-/*
- *
- * 配置为服务器
- *
-*/
-/*=======================================*/
-
-void WIFI::confServer(byte mode, int port) {
-    SERIAL_WIFI.print("AT+Server=");  //发送AT指令
-    SERIAL_WIFI.print(String(mode));
-    SERIAL_WIFI.print(",");
-    SERIAL_WIFI.println(String(port));
-
-    String data;
-    while (1) {
-     if(SERIAL_WIFI.available()>0) {
-     char a =SERIAL_WIFI.read();
-     data=data+a;
-     }
-     if (data.indexOf("done")!=-1 || data.indexOf("Link is builded")!=-1) {
-         break;
-     }
-  }
+char* WIFI::wget(char* url) {
+    static char buf[256];
+    //TODO: (!url.startsWith("http://")
+    char* host = url+7;
+    // TODO: port numbers
+    char* first_slash = strchr(host, '/');
+    *first_slash = '\0';
+    char* path = first_slash + 1;
+    
+    debug_println(host);
+    debug_println(path);
+    tcp_connect(host,80);
+    sprintf(buf,"GET /%s HTTP/1.0\nHost: %s\n\n", path, host);
+    //sprintf(buf,"GET /%s HTTP/1.0\r\n\r\n", path);
+    send(buf);
+    expect("FUN");
 }
--- uarlWIFI/uarlWIFI.h
+++ uarlWIFI/uarlWIFI.h
@@ -8,13 +8,7 @@
 #define __UARLWIFI_H__
 #include <Arduino.h>
 
-#ifndef SERIAL_WIFI
-#define SERIAL_WIFI Serial
-#endif
-
-// for commands with multiple success states, i just wait until the thing has said all it wants to say, then check
-// (a more correct implementation would be a state machine based on multiple expected strings, but meh)
-// anyway, this is the timeout for that wait
+// timeout to wait for 'success' message when using expect() -- timeout implies failure of a command
 #define DEFAULT_EXPECT_TIMEOUT 2000
 
 // port-level timeout (.setTimeout())
@@ -48,36 +42,29 @@ typedef enum {
 
 class WIFI {
   public:
-    Stream* dbg;
-
-    WIFI(void);
+    WIFI();
+    
+    void begin(HardwareSerial& serial);
     
     void set_debug_stream(Stream& s);
 	
 	bool Initialize(wifi_mode_t mode, char* ssid, char* pwd, byte channel = 1, wifi_enc_t enc = WAP_PSK);
 	//void ipConfig(byte type, String addr, int port, boolean a = 0, byte id = 0);
 	
-	void Send(String str);  // Send
-	void Send(byte id, String str);  //多路模式发送数据
-		
-	int ReceiveMessage(char *buf, int MsgLen = 0);
-	
+    bool command_cr(char* cmd, char* exp1, char* exp2=NULL, int timeout=DEFAULT_EXPECT_TIMEOUT);
     bool command(char* cmd, char* exp1, char* exp2=NULL, int timeout=DEFAULT_EXPECT_TIMEOUT);
     bool expect(char* exp1, char* exp2=NULL, int timeout=DEFAULT_EXPECT_TIMEOUT);
     
-    /*================TCP/IP指令================*/
-    String showStatus(void);    //查询链接状态
-    String showMux(void);       //查询目前的链接模式（单链接or多链接）
-    void confMux(boolean a);    //设置链接模式（单链接【0】or多链接【1】）
-    void newMux(byte type, String addr, int port);   //创建 tcp或者udp链接（单路模式)
-    void newMux(byte id, byte type, String addr, int port);   //创建 tcp或者udp链接（多路模式)（id:0-4） 0号可以连接客户端和服务器，其他只能连接服务器
-    void closeMux(void);   //关闭tcp或udp （单路模式）
-    void closeMux(byte id); //关闭tcp货udp （多路模式）
-    String showIP(void);    //显示本机目前获取的ip地址
-    void confServer(byte mode, int port);  //配置服务器
-	
-	String m_rev;
+    bool tcp_connect(char* ip_address, unsigned short port);
     
+    bool send(char* data);
+    char* wget(char* url);
+    
+  private:
+  
+    HardwareSerial* SerialWifi;
+    Stream* dbg;
+
 };
 
 #endif
